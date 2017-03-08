d14bd59a1263db3105e357310a5e912b99e85844
Andrew Burnheimer <Andrew.Burnheimer@nbcuni.com>
2017-03-07 22:33:43 -0500

    Implemented SimplifiedHttpResponse, finishing off web interaction

--- StudioSentry/Service.vb
+++ StudioSentry/Service.vb
@@ -4,6 +4,9 @@ Imports System.Text
 Imports System.Threading
 Imports Newtonsoft.Json
 
+Public Class ServiceMetadata
+    Public Shared version_num As String = "0.0.9"
+End Class
 
 ' Threading example found at http://stackoverflow.com/questions/27926103/vb-net-tcplistner-windows-service#question
 Public Class Service
@@ -69,19 +72,44 @@ Public Class Service
                 Dim clientRequest As New SimplifiedHttpRequest(msg)
                 ev.WriteEntry("Req from client: " & clientRequest.toString())
 
-                'Send back:
-                'Http/1.1 200 OK
-                'Date: Sun, 08 Feb 2018 01:11:12 GMT
-                'Server: Apache/1.3.29 (Win32)
-                'Content-Length: 35
-                'Connection: close
-                'Content-Type: application/json
-                '
-                '{}
+                Dim serverResponse As SimplifiedHttpResponse
+                Try
+                    If clientRequest.resource = "/about" Then
+                        Dim ar As New AboutResponse
+                        serverResponse = New SimplifiedHttpResponse(200, JsonConvert.SerializeObject(ar))
+                    ElseIf clientRequest.resource = "/usage" Then
+                        Dim ur As New UsageResponse
+                        serverResponse = New SimplifiedHttpResponse(200, JsonConvert.SerializeObject(ur))
+                    ElseIf clientRequest.resource = "/NumOfRemoteAppUsers" Then
+                        Dim rr As New RemoteAppUsersResponse
+                        rr.NumOfRemoteAppUsers = NumOfRemoteAppUsers()
+                        serverResponse = New SimplifiedHttpResponse(200, JsonConvert.SerializeObject(rr))
+                    ElseIf clientRequest.resource = "/NumOfRemoteDesktopUsers" Then
+                        Dim rr As New RemoteDesktopUsersResponse
+                        rr.NumOfRemoteDesktopUsers = NumOfRemoteDesktopUsers()
+                        serverResponse = New SimplifiedHttpResponse(200, JsonConvert.SerializeObject(rr))
+                    ElseIf clientRequest.resource = "/KickRemoteAppUser" And clientRequest.method = "PUT" Then
+                        KickRemoteAppUser()
+                        serverResponse = New SimplifiedHttpResponse(202)
+                    Else
+                        Dim ur As New UsageResponse(False)
+                        serverResponse = New SimplifiedHttpResponse(404, JsonConvert.SerializeObject(ur))
+                    End If
 
-                'Dim serverResponse As String = "Response to send"
-                'Dim sendBytes As [Byte]() = Encoding.ASCII.GetBytes(serverResponse)
-                'clientStream.Write(sendBytes, 0, sendBytes.Length)
+                Catch ex As Exception
+                    Dim code As Integer = 500
+                    If TypeOf ex Is NotImplementedException Then
+                        code = 501
+                    End If
+                    Dim er As New ErrorResponse(code, ex.Message, ex.StackTrace)
+                    serverResponse = New SimplifiedHttpResponse(code, JsonConvert.SerializeObject(er))
+                End Try
+
+
+                ev.WriteEntry("Res to client: " & serverResponse.toString())
+
+                Dim sendBytes As [Byte]() = Encoding.ASCII.GetBytes(serverResponse.payload)
+                clientStream.Write(sendBytes, 0, sendBytes.Length)
             End While
         Catch ex As Exception
             ev.WriteEntry("Client Comm. thread exception: " & ex.ToString, EventLogEntryType.Warning)
@@ -112,10 +140,60 @@ Public Class Service
     End Function
 
     Private Function ListOfRemoteAppUsers() As String()
+        Throw New NotImplementedException()
+        Return New String(0) {}
+    End Function
+
+    Private Function NumOfRemoteDesktopUsers() As Integer
+        Return ListOfRemoteDesktopUsers().Count
+    End Function
+
+    Private Function ListOfRemoteDesktopUsers() As String()
+        Throw New NotImplementedException()
         Return New String(0) {}
     End Function
 
     Private Sub KickRemoteAppUser()
+        Throw New NotImplementedException()
+    End Sub
+End Class
+
+Public Class UsageResponse
+    Public status As Integer = 200
+    Public message As String = "OK"
+
+    Public validEndpoints(,) As String = {{"GET", "/about"}, {"GET", "/usage"}, {"GET", "/NumOfRemoteAppUsers"},
+        {"GET", "/NumOfRemoteDesktopUsers"}, {"PUT", "/KickRemoteAppUser"}}
+    Public Sub New(Optional intentionalCall As Boolean = True)
+        If Not intentionalCall Then
+            message = "Resource unknown, validEndpoints provided below"
+            status = 404
+        End If
 
     End Sub
+End Class
+
+Public Class ErrorResponse
+    Public status As Integer
+    Public message As String
+    Public strace As String
+
+    Public Sub New(s As Integer, m As String, Optional st As String = "")
+        status = s
+        message = m
+        strace = st
+    End Sub
+End Class
+
+Public Class AboutResponse
+    Public name As String = "StudioSentry"
+    Public version As String = ServiceMetadata.version_num
+End Class
+
+Public Class RemoteAppUsersResponse
+    Public NumOfRemoteAppUsers As Integer
+End Class
+
+Public Class RemoteDesktopUsersResponse
+    Public NumOfRemoteDesktopUsers As Integer
 End Class
---
+++ StudioSentry/SimplifiedHttpResponse.vb
@@ -0,0 +1,76 @@
+﻿Public Class SimplifiedHttpResponse
+    Private protocol As String = "HTTP"
+    Private version As String = "1.1"
+    Private code As Integer
+    Private reasonPhrase As String
+    Private headers As New Dictionary(Of String, String) From {{"Connection", "close"}}
+
+    Private body As String
+
+    Public Sub New(status As Integer, Optional responseBody As String = "", Optional contentType As String = "application/json")
+        body = responseBody
+        code = status
+        reasonPhrase = setReasonPhrase(status)
+        setHeader("Content-Type", contentType)
+        setHeader("Server", "StudioSentry/" & ServiceMetadata.version_num & " (.NET 4)")
+        setContentLengthHeader()
+        setDateHeader()
+    End Sub
+
+    Overrides Function toString() As String
+        Return "<SimplifiedHttpResponse " & protocol & "/" & version & " " & code & " " & reasonPhrase & ", " & body.Length & " bytes, " & headers.Count & " hdrs>"
+    End Function
+
+    Function payload() As String
+        Dim data As String = ""
+        data += protocol & "/" & version & " " & code & " " & reasonPhrase & vbCrLf
+        For Each kvp As KeyValuePair(Of String, String) In headers
+            Dim k As String = kvp.Key
+            Dim v As String = kvp.Value
+            data += k & ": " & v & vbCrLf
+        Next
+        data += vbCrLf ' empty line feed separating headers from body
+        data += body
+        Return data
+    End Function
+
+    Public Sub setHeader(title As String, value As String)
+        deleteHeader(title)
+        addHeader(title, value)
+    End Sub
+
+    Private Sub setDateHeader()
+        setHeader("Date", DateTime.UtcNow.ToString("ddd, dd MMM yyyy HH:mm:ss") & " GMT")
+    End Sub
+
+    Private Function setReasonPhrase(status As Integer) As String
+        Dim message As String = ""
+        If status = 200 Then
+            message = "OK"
+        ElseIf status = 202 Then
+            message = "ACCEPTED"
+        ElseIf status = 404 Then
+            message = "NOT FOUND"
+        ElseIf status = 501 Then
+            message = "NOT IMPLEMENTED"
+        ElseIf status = 500 Then
+            message = "INTERNAL SERVER ERROR"
+        Else
+            message = "UNKNOWN STATUS"
+        End If
+        Return message
+    End Function
+
+    Private Sub addHeader(title As String, value As String)
+        headers.Add(title, value)
+    End Sub
+
+    Private Sub deleteHeader(title As String)
+        headers.Remove(title)
+    End Sub
+
+    Private Function setContentLengthHeader() As Integer
+        setHeader("Content-Length", body.Length)
+        Return body.Length
+    End Function
+End Class
